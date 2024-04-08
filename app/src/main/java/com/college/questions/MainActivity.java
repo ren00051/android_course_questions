73064289e64f872fa208d156fc7aaee9453f6189
Shanshan Ren <ren00051@algonquinlive.com>
2024-04-08 00:33:45 -0400

    completed todo.

--- app/src/main/java/com/college/questions/MainActivity.java
+++ app/src/main/java/com/college/questions/MainActivity.java
@@ -6,6 +6,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.college.questions.databinding.ActivityMainBinding;
+
+import org.w3c.dom.Text;
+
 import java.util.Locale;
 
 /*
@@ -16,17 +20,28 @@ public class MainActivity extends AppCompatActivity {
 
     static private final String TAG = "MainActivity";
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button  addButton  = findViewById(R.id.add_button);
-        TextView tvResult = findViewById(R.id.operation_result);
+
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //Button  addButton  = findViewById(R.id.add_button);
+        Button addButton = binding.addButton;
+
+        //TextView tvResult = findViewById(R.id.operation_result);
+        TextView tvResult = binding.operationResult;
 
         addButton.setOnClickListener( (v) -> {
-            EditText operandOne = findViewById(R.id.operand_one_edit);
-            EditText operandTwo  = findViewById(R.id.operand_two_edit);
+            //EditText operandOne = findViewById(R.id.operand_one_edit);
+            EditText operandOne = binding.operandOneEdit;
+            //EditText operandTwo  = findViewById(R.id.operand_two_edit);
+            EditText operandTwo = binding.operandTwoEdit;
 
             String firstNumber = operandOne.getText().toString();
             String secondNumber = operandTwo.getText().toString();
